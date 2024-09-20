2f6f7a8691d8bf7cc601dfbc324ba129d80957f8
Chloe Lin <ceIery@users.noreply.github.com>
2024-09-20 02:02:59 -0400

    refactor main/index.js
    - remove unnecessary logic
    - move team/point logic to gameplay
    - migrate to websocket v2 endpoint

--- gameplay/index.js
+++ gameplay/index.js
@@ -1,16 +1,68 @@
 const socket = new ReconnectingWebSocket('ws://' + location.host + '/websocket/v2');
-const cache = {};
+socket.onopen = () => { console.log('Successfully Connected'); };
+socket.onclose = event => { console.log('Socket Closed Connection: ', event); socket.send('Client Closed!'); };
+socket.onerror = error => { console.log('Socket Error: ', error); };
+
+let starsVisible, bestOf;
+let starsRed, starsBlue, nameRed, nameBlue;
 
 socket.onmessage = async event => {
     const data = JSON.parse(event.data);
+    if (nameRed !== data.tourney.team.left) {
+		nameRed = data.tourney.team.left || 'Red Team';
+		$('#red_name').text(nameRed);
+	}
+	if (nameBlue !== data.tourney.team.right) {
+		nameBlue = data.tourney.team.right || 'Blue Team';
+		$('#blue_name').text(nameBlue);
+	}
 
-    if (cache.scoreVisible !== data.tourney.scoreVisible) {
-        cache.scoreVisible = data.tourney.scoreVisible;
+    if (starsVisible !== data.tourney.starsVisible) {
+		starsVisible = data.tourney.starsVisible;
+		if (starsVisible) {
+			$('#blue_points').css('opacity', 1);
+			$('#red_points').css('opacity', 1);
 
-        if (cache.scoreVisible) {
-            $('#bottomright').addClass('blue').removeClass('red');
-        } else {
-            $('#bottomright').addClass('red').removeClass('blue')
-        }
-    }
+		} else {
+			$('#blue_points').css('opacity', 0);
+			$('#red_points').css('opacity', 0);
+		}
+	}
+
+    if (bestOf !== data.tourney.bestOF) {
+		const newmax = Math.ceil(data.tourney.bestOF / 2);
+		if (bestOf === undefined) {
+			for (let i = 1; i <= newmax; i++) {
+				$('#red_points').append($('<div></div>').attr('id', `red${i}`).addClass('team-point red'));
+				$('#blue_points').append($('<div></div>').attr('id', `blue${i}`).addClass('team-point blue'));
+			}
+		}
+		else if (bestOf < data.tourney.bestOF) {
+			for (let i = firstTo + 1; i <= newmax; i++) {
+				$('#red_points').append($('<div></div>').attr('id', `red${i}`).addClass('team-point red'));
+				$('#blue_points').append($('<div></div>').attr('id', `blue${i}`).addClass('team-point blue'));
+			}
+		}
+		else {
+			for (let i = firstTo; i > newmax; i--) {
+				$(`#red${i}`).remove();
+				$(`#blue${i}`).remove();
+			}
+		}
+		bestOf = data.tourney.bestOF;
+		firstTo = newmax;
+	}
+
+    if (starsRed !== data.tourney.points.left) {
+		starsRed = data.tourney.points.left;
+		for (let i = 1; i <= starsRed; i++) { $(`#red${i}`).addClass('filled'); }
+		for (let i = starsRed + 1; i <= firstTo; i++) { $(`#red${i}`).removeClass('filled'); }
+	}
+
+	if (starsBlue !== data.tourney.points.right) {
+		starsBlue = data.tourney.points.right;
+		for (let i = 1; i <= starsBlue; i++) { $(`#blue${i}`).addClass('filled'); }
+		for (let i = starsBlue + 1; i <= firstTo; i++) { $(`#blue${i}`).removeClass('filled'); }
+	}
+    
 }
--- main/index.js
+++ main/index.js
@@ -6,27 +6,8 @@ let mappool, teams;
 	mappool = await $.getJSON('../_data/beatmaps.json');
 	teams = await $.getJSON('../_data/teams.json');
 	let stage = mappool.stage;
-	if (stage) $('#stage_name').text(stage);
-	carousel();
+	if (stage) $('.stage').text(stage);
 })();
-
-const carousel = () => {
-	const total_items = 4;
-	let item_id = 1;
-	$(`#middle_item_${item_id}`).css('opacity', 1);
-	$(`#middle_item_${item_id}`).css('display', 'flex');
-
-	setInterval(async () => {
-		$(`#middle_item_${item_id}`).css('opacity', 0);
-		await delay(300);
-		$(`#middle_item_${item_id}`).css('display', 'none');
-		item_id++;
-		if (item_id > total_items) item_id = 1;
-		$(`#middle_item_${item_id}`).css('display', 'flex');
-		await delay(100);
-		$(`#middle_item_${item_id}`).css('opacity', 1);
-	}, 7000);
-}
 
 let map, mapid;
 window.setInterval(async () => {
@@ -69,7 +50,7 @@ window.setInterval(async () => {
 	}
 }, 500);
 
-let socket = new ReconnectingWebSocket('ws://' + location.host + '/ws');
+let socket = new ReconnectingWebSocket('ws://' + location.host + '/websocket/v2');
 
 let animation = {
 	red_score: new CountUp('score_red', 0, 0, 0, .3, { useEasing: true, useGrouping: true, separator: ',', decimal: '.', suffix: '' }),
@@ -96,135 +77,46 @@ socket.onmessage = async event => {
 	const data = JSON.parse(event.data);
 	const now = Date.now();
 
-	if (scoreVisible !== data.tourney.manager.bools.scoreVisible) {
-		scoreVisible = data.tourney.manager.bools.scoreVisible;
+	if (scoreVisible !== data.tourney.scoreVisible) {
+		scoreVisible = data.tourney.scoreVisible;
 
 		if (scoreVisible) {
 			$('#chat_container').css('opacity', 0);
 			$('#top_footer').css('opacity', 1);
-			$('#title').css('width', '1000px');
 		} else {
 			$('#chat_container').css('opacity', 1);
 			$('#top_footer').css('opacity', 0);
-			$('#title').css('width', '654px');
 		}
 	}
 
-	if (starsVisible !== data.tourney.manager.bools.starsVisible) {
-		starsVisible = data.tourney.manager.bools.starsVisible;
-		if (starsVisible) {
-			$('#blue_points').css('opacity', 1);
-			$('#red_points').css('opacity', 1);
-
-		} else {
-			$('#blue_points').css('opacity', 0);
-			$('#red_points').css('opacity', 0);
-		}
-	}
-
-	if (teams && nameRed !== data.tourney.manager.teamName.left) {
-		nameRed = data.tourney.manager.teamName.left || 'Red Team';
-		$('#red_name').text(nameRed);
-		let team = teams.find(t => t.team === nameRed);
-
-		$('#red_flag').css('background-image', `url('../_shared/assets/flags/${team?.flag ?? 'XX'}.png')`);
-		$('#red_seed').text(`SEED ${team?.seed ?? 'X'}`);
-	}
-	if (teams && nameBlue !== data.tourney.manager.teamName.right) {
-		nameBlue = data.tourney.manager.teamName.right || 'Blue Team';
-		$('#blue_name').text(nameBlue);
-		let team = teams.find(t => t.team === nameBlue);
-
-		$('#blue_flag').css('background-image', `url('../_shared/assets/flags/${team?.flag ?? 'XX'}.png')`);
-		$('#blue_seed').text(`SEED ${team?.seed ?? 'X'}`);
-	}
-
-	if (bestOf !== data.tourney.manager.bestOF) {
-		const newmax = Math.ceil(data.tourney.manager.bestOF / 2);
-		if (bestOf === undefined) {
-			for (let i = 1; i <= newmax; i++) {
-				$('#red_points').append($('<div></div>').attr('id', `red${i}`).addClass('team-point red'));
-				$('#blue_points').append($('<div></div>').attr('id', `blue${i}`).addClass('team-point blue'));
-			}
-		}
-		else if (bestOf < data.tourney.manager.bestOF) {
-			for (let i = firstTo + 1; i <= newmax; i++) {
-				$('#red_points').append($('<div></div>').attr('id', `red${i}`).addClass('team-point red'));
-				$('#blue_points').append($('<div></div>').attr('id', `blue${i}`).addClass('team-point blue'));
-			}
-		}
-		else {
-			for (let i = firstTo; i > newmax; i--) {
-				$(`#red${i}`).remove();
-				$(`#blue${i}`).remove();
-			}
-		}
-		bestOf = data.tourney.manager.bestOF;
-		firstTo = newmax;
-	}
-
-	if (starsRed !== data.tourney.manager.stars.left) {
-		starsRed = data.tourney.manager.stars.left;
-		for (let i = 1; i <= starsRed; i++) { $(`#red${i}`).addClass('filled'); }
-		for (let i = starsRed + 1; i <= firstTo; i++) { $(`#red${i}`).removeClass('filled'); }
-	}
-
-	if (starsBlue !== data.tourney.manager.stars.right) {
-		starsBlue = data.tourney.manager.stars.right;
-		for (let i = 1; i <= starsBlue; i++) { $(`#blue${i}`).addClass('filled'); }
-		for (let i = starsBlue + 1; i <= firstTo; i++) { $(`#blue${i}`).removeClass('filled'); }
-	}
-
-	if (mappool && md5 !== data.menu.bm.md5) {
-		md5 = data.menu.bm.md5;
+	if (mappool && md5 !== data.beatmap.checksum) {
+		md5 = data.beatmap.checksum;
 		setTimeout(() => { update_stats = true }, 250);
 	}
 
 	if (update_stats) {
 		update_stats = false;
-		mapid = data.menu.bm.id;
-		map = mappool ? mappool.beatmaps.find(m => m.beatmap_id == mapid || m.md5 == md5) ?? { id: data.menu.bm.id, mods: 'NM', identifier: null } : { id: null, mods: 'NM', identifier: null };
+		mapid = data.beatmap.id;
+		map = mappool ? mappool.beatmaps.find(m => m.beatmap_id == mapid || m.md5 == md5) ?? { id: data.beatmap.id, mods: 'NM', identifier: null } : { id: null, mods: 'NM', identifier: null };
 		const mods = map.mods ?? 'NM';
-		const stats = getModStats(data.menu.bm.stats.memoryCS, data.menu.bm.stats.memoryAR, data.menu.bm.stats.memoryOD, data.menu.bm.stats.BPM.common, mods);
-		const len_ = data.menu.bm.time.full - data.menu.bm.time.firstObj;
+		const stats = getModStats(data.beatmap.stats.cs.original, data.beatmap.stats.ar.original, data.beatmap.stats.od.original, data.beatmap.stats.bpm.common, mods);
+		const len_ = data.beatmap.time.lastObject - data.beatmap.time.firstObject;
 
-		$('#cs').text(stats.cs);
-		$('#ar').text(stats.ar);
-		$('#od').text(stats.od);
-		$('#bpm').text(map?.bpm ?? stats.bpm);
-		$('#length').text(`${Math.trunc((len_ / stats.speed) / 1000 / 60)}:${Math.trunc((len_ / stats.speed) / 1000 % 60).toString().padStart(2, '0')}`);
-		$('#sr').text(`${map?.sr ?? data.menu.bm.stats.fullSR}★`);
+		$('#cs').text(`cs ${stats.cs}`);
+		$('#ar').text(`ar ${stats.ar}`);
+		$('#od').text(`od ${stats.od}`);
+		$('#bpm').text(`${stats.bpm} bpm`);
+		$('#length').text(`${Math.trunc((len_ / stats.speed) / 1000 / 60)}:${Math.trunc((len_ / stats.speed) / 1000 % 60).toString().padStart(2, '0')} length`);
+		$('#sr').text(`${data.beatmap.stats.stars.total} stars`);
 
-		$('#title').text(`${data.menu.bm.metadata.artist} - ${data.menu.bm.metadata.title} [${data.menu.bm.metadata.difficulty}]`);
-
-		// map.identifier = 'HD2';
-		if (map?.identifier) {
-			$('#map_slot_container').css('display', 'flex').css('opacity', 1);
-			await delay(10);
-			$('#map_slot_container').css('width', '50px');
-			$('#map_slot').text(map.identifier).css('opacity', 1);
-		}
-		else {
-			$('#map_slot').css('opacity', 0);
-			await delay(200);
-			$('#map_slot_container').css('width', '0px');
-			await delay(300);
-			$('#map_slot_container').css('display', 'none').css('opacity', 0);
-			$('#map_slot').text('');
-		}
-	}
-
-	if (image !== data.menu.bm.path.full) {
-		image = data.menu.bm.path.full;
-		data.menu.bm.path.full = data.menu.bm.path.full.replace(/#/g, '%23').replace(/%/g, '%25').replace(/\\/g, '/').replace(/'/g, `\\'`);
-		$('#map_image').css('background-image', `url('http://${location.host}/Songs/${data.menu.bm.path.full}')`);
+		$('#title').text(`${data.beatmap.artist} - ${data.beatmap.title} [${data.beatmap.version}]`);
 	}
 
 	if (scoreVisible) {
 		let scores = [];
 		for (let i = 0; i < TEAMSIZE * 2; i++) {
-			let score = data.tourney.ipcClients[i]?.gameplay?.score || 0;
-			if (data.tourney.ipcClients[i]?.gameplay?.mods?.str?.toUpperCase().includes('EZ')) score *= 1.75;
+			let score = data.tourney.clients[i]?.play?.score || 0;
+			if (data.tourney.clients[i]?.play?.mods?.name?.toUpperCase().includes('EZ')) score *= 1.75;
 			scores.push({ id: i, score });
 		}
 
@@ -274,86 +166,35 @@ socket.onmessage = async event => {
 		}
 	}
 
-	if (chatLen !== data.tourney.manager.chat.length) {
+	if (chatLen !== data.tourney.chat.length) {
 
-		const current_chat_len = data.tourney.manager.chat.length;
+		const current_chat_len = data.tourney.chat.length;
 		if (chatLen === 0 || (chatLen > 0 && chatLen > current_chat_len)) { $('#chat').html(''); chatLen = 0; }
 
 		for (let i = chatLen; i < current_chat_len; i++) {
-			const chat = data.tourney.manager.chat[i];
-			const body = chat.messageBody;
-			const time = chat.time;
-			if (body.toLowerCase().startsWith('!mp')) {
-				if (first_chat_refresh) continue;
-				const command = body.toLowerCase();
-				const command_value = Number(command.match(/\d+/)) ?? 0;
+			const chat = data.tourney.chat[i];
+			const body = chat.message;
+			const time = chat.timestamp;
 
-				if (command.startsWith('!mp timer')) {
-					if (isNaN(command_value)) { stop_timer(); continue; }
-					else start_timer(command_value);
-				}
-				if ((command.startsWith('!mp aborttimer') && command.startsWith('!mp start')) && timer_in_progress) stop_timer();
-			}
-			else {
-				const team = team_lookup[chat.team] ?? 'unknown';
-				const player = chat.name;
-				if (player === 'BanchoBot' && body.startsWith('Match history')) continue;
+			const team = team_lookup[chat.team] ?? 'unknown';
+			const player = chat.name;
+			if (player === 'BanchoBot' && body.startsWith('Match history')) continue;
 
-				team_actual = teams.find(t => t.players.map(p => p.username).includes(player))?.team;
-				teamcode_actual = team_actual ? team_actual === nameRed ? 'red' : team_actual === nameBlue ? 'blue' : null : null;
+			team_actual = teams.find(t => t.players.map(p => p.username).includes(player))?.team;
+			teamcode_actual = team_actual ? team_actual === nameRed ? 'red' : team_actual === nameBlue ? 'blue' : null : null;
 
-				const chatParent = $('<div></div>').addClass(`chat-message ${teamcode_actual ?? team}`);
+			const chatParent = $('<div></div>').addClass(`chat-message ${teamcode_actual ?? team}`);
 
-				chatParent.append($('<div></div>').addClass('chat-time').text(time));
-				chatParent.append($('<div></div>').addClass('chat-name').text(player));
-				chatParent.append($('<div></div>').addClass('chat-body').text(body));
+			chatParent.append($('<div></div>').addClass('chat-time').text(time));
+			chatParent.append($('<div></div>').addClass('chat-name').text(player));
+			chatParent.append($('<div></div>').addClass('chat-body').text(body));
 
-				$('#chat').prepend(chatParent);
-			}
+			$('#chat').prepend(chatParent);
 		}
 
-		chatLen = data.tourney.manager.chat.length;
+		chatLen = data.tourney.chat.length;
 		first_chat_refresh = false;
 	}
-}
-
-const really_start_timer = length => {
-	timer_in_progress = true;
-	$('#timer_progress').css('animation', 'none');
-	$('#stopwatch_container').css('animation', 'none');
-	$('#timer_container').css('transform', 'translateY(0px)');
-	$('#timer_progress').css('animation', `progress ${length}s linear`);
-
-	if (length > 30) {
-		blink_timer = setTimeout(() => {
-			if (!timer_in_progress) return;
-			$('#timer_progress').css('animation', `progress ${length}s linear, progress_blink 0.7s infinite ease`);
-			$('#stopwatch_container').css('animation', `progress_blink 0.7s infinite ease`);
-		}, (length - 3) * 1000);
-	}
-
-	timer = setTimeout(() => {
-		if (!timer_in_progress) return;
-		stop_timer();
-	}, length * 1000);
-}
-
-const start_timer = length => {
-	window.requestAnimationFrame(() => {
-		stop_timer();
-		window.requestAnimationFrame(() => {
-			really_start_timer(length);
-		})
-	})
-}
-
-const stop_timer = () => {
-	clearTimeout(timer);
-	clearTimeout(blink_timer);
-	timer_in_progress = false;
-	$('#timer_progress').css('animation', 'none');
-	$('#stopwatch_container').css('animation', 'none');
-	$('#timer_container').css('transform', 'translateY(32px)');
 }
 
 const team_lookup = {
